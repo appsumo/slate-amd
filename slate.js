6f830c8b945fa6a8d8e374a6d7dca0729416a5e7
Robert Dale Smith <robert@appsumo.com>
2017-11-09 18:43:16 -0600

    adds appsumo/slate-markdown-serializer

--- slate.js
+++ slate.js
@@ -2,4 +2,5 @@ module.exports = {
   'slate': require('slate'),
   'slate-react': require('slate-react'),
   'slate-plain-serializer': require('slate-plain-serializer'),
+  'slate-markdown-serializer': require('slate-markdown-serializer').default,
 };
