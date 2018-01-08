d004640ca40e93536ab7a79847f25550b3f9d71f
Robert Dale Smith <robert@appsumo.com>
2018-01-08 14:18:40 -0600

    feat(slate) - updates slate version 0.32.0

--- slate.js
+++ slate.js
@@ -1,6 +1,6 @@
 module.exports = {
   'slate': require('slate'),
   'slate-react': require('slate-react'),
-  'slate-plain-serializer': require('slate-plain-serializer'),
+  'slate-plain-serializer': require('slate-plain-serializer').default,
   'slate-markdown-serializer': require('slate-markdown-serializer').default,
 };
