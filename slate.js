985f57a8e2187b39c54c106e7b051b491551c19f
Robert Dale Smith <robert@appsumo.com>
2019-01-08 17:37:08 -0600

    feat - bundles slate-html-serializer for client side

--- slate.js
+++ slate.js
@@ -1,5 +1,7 @@
 module.exports = {
   'slate': require('slate'),
+  'slate-react': require('slate-react'),
+  'slate-html-serializer': require('slate-html-serializer').default,
   'slate-plain-serializer': require('slate-plain-serializer').default,
   'slate-markdown-serializer': require('slate-markdown-serializer').default,
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
 module.exports = {
   entry: "./slate.js",
-    output: {
-        library: 'slate-lib',
-        libraryTarget: 'amd',
-        filename: 'slate-lib.js'
-    },
+  output: {
+    library: 'slate-lib',
+    libraryTarget: 'amd',
+    filename: 'slate-lib.js'
+  },
   resolve: {
     modules: [
       'node_modules',
       'packages'
     ]
   },
-    externals: [
-        {
-            "react": {
-                root: "React",
-                commonjs2: "react",
-                commonjs: "react",
-                amd: "react"
-            },
-            "react-dom": {
-                root: "ReactDOM",
-                commonjs2: "react-dom",
-                commonjs: "react-dom",
-                amd: "react-dom"
-            }
-   }
+  externals: [
+    {
+      "react": {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      },
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs2: "react-dom",
+        commonjs: "react-dom",
+        amd: "react-dom"
+      }
+    }
   ],
   module: {
-         loaders: [
-             {
-                 test: /\.js$/,
-                 loader: 'babel-loader',
-                 query: {
-                     presets: ['es2015']
-                 }
-             }
-         ]
-     }
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
 };
